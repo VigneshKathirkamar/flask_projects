1d948c012a7a62ecfdd42a80f13cde2c2117908a
Vignesh Kathirkmar <i51vignesh@gmail.com>
2022-03-26 00:36:14 +0530

    for loop usage in flask

---
+++ tut_2/Readme.md
@@ -0,0 +1,1 @@
+# Usage of for loop in flask
---
+++ tut_2/main.py
@@ -0,0 +1,18 @@
+from distutils.log import debug
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route('/',methods=["POST","GET"])
+def home():
+    sum = ''
+    first_num = 0
+    second_num = 0
+    if request.method == "POST" and 'first_num' in request.form and 'second_num' in request.form:
+        first_num = int(request.form.get('first_num'))
+        second_num = int(request.form.get('second_num'))
+        sum = float(first_num + second_num)
+    return render_template('index.html',sum=sum,first_num=first_num,second_num=second_num)
+
+if __name__ == "__main__":
+    app.run(debug=True)
